48f708282301c861488d73c57ac97669c08fb573
Muhammad Talha Dar <talha.dar@invozone.com>
2022-10-27 23:34:24 +0500

    some bug fixes

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 // 2.   We should be able to enter a value on the terminal.
 // 3.   Current value and previous values should be return when we hit ENTER key.
 
-use std::io::{self, Read, Write};
+use std::io::{self, Write};
 use std::vec::Vec;
 
 struct TodoApp {
@@ -10,47 +10,50 @@ struct TodoApp {
 }
 
 pub trait ReadLine {
-    fn read_line(&mut self) -> Result<usize, ()>;
+    fn read_line(&mut self) -> Option<usize>;
 }
 
 impl ReadLine for TodoApp {
-    fn read_line(&mut self) -> Result<usize, ()> {
+    fn read_line(&mut self) -> Option<usize> {
+        // get the input and flush
         print!("Enter a new value: ");
         io::stdout().flush().unwrap();
 
+        // make temporary buffer for StdIn buff
         let mut buff = String::new();
         let stdin = io::stdin();
 
-        let size = stdin.read_line(&mut buff);
-        // buff.truncate(size - 2);
-        match size {
-            Ok(val) => {
-                buff.truncate(val - 2);
-                self.list.push(buff.clone());
-                return Ok(buff.len())
-            },
-            Err(_) => return Err(())
-        }
+        // read line
+        match stdin.read_line(&mut buff) {
+            Err(_) => return None,
+            Ok(size) => {
+                buff.truncate(buff.len() - 1);
+                match buff.is_empty() {
+                    true => (),
+                    false => self.list.push(buff),
+                }
+                return Some(size);
+            }
+        };
     }
 }
 
-impl TodoApp{
-    fn print_list(&self){
+impl TodoApp {
+    fn print_list(&self) {
         println!("Returns: {:?}", self.list);
         io::stdout().flush().unwrap();
     }
 }
 
 fn main() {
-    let mut test_input: String = String::new();
-    let mut todo_list: Vec<String> = vec![];
-    let mut todo_app: TodoApp = TodoApp { list: vec![]};
+    let mut todo_app: TodoApp = TodoApp { list: vec![] };
 
-    // let stdin = io::stdin();
     loop {
         // read input
-        todo_app.read_line().unwrap();
-
+        match todo_app.read_line() {
+            None => break,
+            Some(_) => (),
+        };
         // print out the list
         todo_app.print_list();
     }
